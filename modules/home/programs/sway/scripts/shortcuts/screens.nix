06f61f3c752c0f8d374a0fd0a8af05e90eb573a4
Jackson Warhover <11669560+jbwar22@users.noreply.github.com>
2025-03-13 11:23:09 -0400

    screen reset shortcut

--- modules/home/programs/sway/scripts/shortcuts/screens.nix
+++ modules/home/programs/sway/scripts/shortcuts/screens.nix
@@ -4,4 +4,7 @@ pkgs: lib: config:
   vert = pkgs.writeShellScript "shortcuts-screens-vert" ''
     ${pkgs.sway}/bin/swaymsg 'output "ASUSTek COMPUTER INC VG27AQL1A S1LMQS102258" transform 90'
   '';
+  reset = pkgs.writeShellScript "shortcuts-screens-reset" ''
+    ${pkgs.sway}/bin/swaymsg 'output "ASUSTek COMPUTER INC VG27AQL1A S1LMQS102258" transform 0'
+  '';
 }
