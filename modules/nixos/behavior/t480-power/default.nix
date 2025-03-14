9398ed0312000aec4d4d1e63299a598f1c99e29d
Jackson Warhover <11669560+jbwar22@users.noreply.github.com>
2025-03-14 17:26:52 -0400

    performance on AC

--- modules/nixos/behavior/t480-power/default.nix
+++ modules/nixos/behavior/t480-power/default.nix
@@ -8,13 +8,13 @@ with lib; with ns config ./.; {
   config = mkIf cfg.enable {
     services.tlp = {
       enable = true;
-      settings = let 
-        thresh = foldl' (accum: x:
-          accum // {
-            "START_CHARGE_THRESH_${x.name}" = x.value.min;
-            "STOP_CHARGE_THRESH_${x.name}" = x.value.max;
-          }) {} (attrsToList config.custom.common.opts.hardware.batteries);
-      in {} // thresh;
+      settings = foldl' (accum: x: {
+        "START_CHARGE_THRESH_${x.name}" = x.value.min;
+        "STOP_CHARGE_THRESH_${x.name}" = x.value.max;
+      }) {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      } (attrsToList config.custom.common.opts.hardware.batteries);
     };
 
     services.thermald.enable = true;
