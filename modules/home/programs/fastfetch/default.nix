02156f6439c36372e0575b67a2cbd64378683e83
Jackson Warhover <11669560+jbwar22@users.noreply.github.com>
2025-03-29 17:12:16 -0400

    fastfetch logo

--- modules/home/programs/fastfetch/default.nix
+++ modules/home/programs/fastfetch/default.nix
@@ -1,9 +1,16 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 with lib; mkNsEnableModule config ./. {
   programs.fastfetch = {
     enable = true;
     settings = {
+      logo = {
+        type = "file";
+        source = pkgs.fetchurl {
+          url = "https://raw.githubusercontent.com/4DBug/nix-ansi/bea824b1f863a4b0ee5e8c8f5c5e92e8207b3705/nix.txt";
+          hash = "sha256-K1qBJHAl+sTeeB1WrNbOWf2a/6QAWAXXPiSC2XFHI2E=";
+        };
+      };
       modules = [
         "title"
 
@@ -21,7 +28,7 @@ with lib; mkNsEnableModule config ./. {
         "physicalmemory"
         "display"
         "battery"
-        "disk"
+        # "disk"
 
         "separator"
 
