b97d0e8c8896ff1aa10614acafa6ba2cef62ed81
Jackson Warhover <11669560+jbwar22@users.noreply.github.com>
2025-04-27 10:38:29 -0400

    remove propagated opt

--- modules/nixos/opts/propagated/default.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, ... }:
-
-with lib; with ns config ./.; {
-  options = opt (mkOption {
-    default = {};
-    type = with types; attrsOf anything;
-    description = "record of setHMOpt settings from nixos to home-manager by user";
-  });
-}
