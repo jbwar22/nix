d58ccce4e6364909b6c883323a3e7eab6bba90b7
Jackson Warhover <11669560+jbwar22@users.noreply.github.com>
2025-05-02 16:08:43 -0400

    fprint in swaylock (and also sudo?)

--- modules/nixos/programs/home/swaylock/default.nix
+++ modules/nixos/programs/home/swaylock/default.nix
@@ -2,5 +2,12 @@
 
 with lib; mkNsEnableModule config ./. {
   security.pam.services.swaylock = {};
+  # security.pam.services.swaylock.fprintAuth = mkIf config.services.fprintd.enable true;
+  security.pam.services.swaylock.text = mkIf config.services.fprintd.enable ''
+    auth [success=1 default=ignore] pam_succeed_if.so service in sudo:su:su-1 tty in :unknown
+    auth sufficient pam_unix.so try_first_pass likeauth nullok
+    auth sufficient pam_fprintd.so
+    auth include login
+  '';
   security.polkit.enable = mkDefault true;
 }
