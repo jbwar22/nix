e86572fef3803b7c215966716e9369f6b2f38882
Jackson Warhover <11669560+jbwar22@users.noreply.github.com>
2025-05-04 16:13:42 -0400

    launch signal fix

--- modules/home/programs/signal/default.nix
+++ modules/home/programs/signal/default.nix
@@ -5,5 +5,26 @@ with lib; mkNsEnableModule config ./. {
     signal-desktop
   ];
 
+  # fix for needing to open it up twice
+  xdg.desktopEntries = {
+    signal-desktop = {
+      name = "Signal";
+      exec = "${pkgs.writeShellScript "launch-signal" ''
+        ${pkgs.signal-desktop}/bin/signal-desktop &
+        ${pkgs.coreutils}/bin/sleep 1
+        ${pkgs.signal-desktop}/bin/signal-desktop $@
+      ''} %U";
+      terminal = false;
+      type = "Application";
+      icon = "signal-desktop";
+      comment = "Private messaging from your desktop";
+      mimeType = [ "x-scheme-handler/sgnl" "x-scheme-handler/signalcaptcha" ];
+      categories = [ "Network" "InstantMessaging" "Chat" ];
+      settings = {
+        StartupWMClass = "signal";
+      };
+    };
+  };
+
   home.persistence = persistUserDirs config [ ".config/Signal" ];
 }
