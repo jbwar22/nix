7c431d5f4f2816ab00eef43aefd43095aba3dfbf
Jackson Warhover <11669560+jbwar22@users.noreply.github.com>
2025-05-10 13:54:12 -0400

    change bg

--- modules/home/users/jackson/monstro/default.nix
+++ modules/home/users/jackson/monstro/default.nix
@@ -47,11 +47,11 @@ with lib; {
               resolution = "1920x1080@144.001Hz";
               position = "0 0";
               adaptive_sync = "off";
-              bg = "${../../../../../secrets/git-crypt/wallpaper/p2up0wv6.png} fill";
+              bg = "${../../../../../secrets/git-crypt/wallpaper/iyxxfe0y.png} fill";
             };
             bar = "bar1080";
             specialisations = {
-              bg2.sway.bg = "${../../../../../secrets/git-crypt/wallpaper/iyxxfe0y.png} fill";
+              bg2.sway.bg = "${../../../../../secrets/git-crypt/wallpaper/p2up0wv6.png} fill";
             };
           };
           "ASUSTek COMPUTER INC VG27AQL1A S1LMQS102258" = {
@@ -60,7 +60,7 @@ with lib; {
               position = "1920 0";
               adaptive_sync = "off";
               transform = "0";
-              bg = "${../../../../../secrets/git-crypt/wallpaper/p2up0wv6.png} fill";
+              bg = "${../../../../../secrets/git-crypt/wallpaper/iyxxfe0y.png} fill";
             };
             bar = "bar1440";
             specialisations = {
@@ -68,21 +68,21 @@ with lib; {
                 transform = "90";
                 position = "1920 -740";
               };
-              bg2.sway.bg = "${../../../../../secrets/git-crypt/wallpaper/iyxxfe0y.png} fill";
+              bg2.sway.bg = "${../../../../../secrets/git-crypt/wallpaper/p2up0wv6.png} fill";
             };
           };
           "BNQ BenQ GW2780 V1J07047SL0" = {
             sway = {
               resolution = "1920x1080@60.000Hz";
               position = "4480 0";
-              bg = "${../../../../../secrets/git-crypt/wallpaper/p2up0wv6.png} fill";
+              bg = "${../../../../../secrets/git-crypt/wallpaper/iyxxfe0y.png} fill";
             };
             bar = "bar1080";
             specialisations = {
               vert.sway = {
                 position = "3360 0";
               };
-              bg2.sway.bg = "${../../../../../secrets/git-crypt/wallpaper/iyxxfe0y.png} fill";
+              bg2.sway.bg = "${../../../../../secrets/git-crypt/wallpaper/p2up0wv6.png} fill";
             };
           };
         };
