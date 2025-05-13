53fa86cbc1e046bb296e8bff2d8ebe10d1049bb7
Jackson Warhover <11669560+jbwar22@users.noreply.github.com>
2025-05-13 13:01:22 -0400

    add btop

--- modules/home/programs/noconfig/tui/default.nix
+++ modules/home/programs/noconfig/tui/default.nix
@@ -3,6 +3,7 @@
 with lib; mkNsEnableModule config ./. {
   home.packages = with pkgs; [
     htop
+    btop
     pulsemixer
     inputs.home-manager.packages.${pkgs.system}.default
     inputs.agenix.packages.${pkgs.system}.default
