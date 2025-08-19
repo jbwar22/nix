ca54286953fbf2709c54b47cfee65eb36c47627e
Jackson Warhover <11669560+jbwar22@users.noreply.github.com>
2025-08-19 16:16:16 -0400

    kitty bugs

--- common/overlays/unstable.nix
+++ common/overlays/unstable.nix
@@ -2,6 +2,7 @@ channels: final: prev: {
   inherit (channels.unstable)
   dunst           # for overriding with my up-to-date fork
   firefox         # up-to-date browsers
+  kitty           # 0.42.1 segfaulting
   librewolf       # up-to-date browsers
   qbittorrent;    # features 
 
