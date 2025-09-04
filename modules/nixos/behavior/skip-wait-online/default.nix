400427732fcd8426ae4ece6ce9900506440a04ea
Jackson Warhover <11669560+jbwar22@users.noreply.github.com>
2025-09-04 14:23:25 -0400

    boot time performance

--- modules/nixos/behavior/skip-wait-online/default.nix
+++ modules/nixos/behavior/skip-wait-online/default.nix
@@ -1,7 +1,7 @@
 { config, lib, ... }:
 
 with lib; mkNsEnableModule config ./. {
-  # enable if having issues with network manager wait online
   systemd.network.wait-online.enable = false;
   boot.initrd.systemd.network.wait-online.enable = false;
+  systemd.services.NetworkManager-wait-online.enable = false;
 }
--- modules/nixos/programs/fwupd/default.nix
+++ modules/nixos/programs/fwupd/default.nix
@@ -1,9 +1,15 @@
 { config, lib, ... }:
 
-with lib; mkNsEnableModule config ./. (let
+with lib; with ns config ./.; (let
   admins = getAdmins config.custom.common.opts.host.users;
 in {
-  services.fwupd.enable = true;
-
-  users = setUserGroups admins [ "input" ];
+  options = opt {
+    enable = mkEnableOption "fwupd";
+    enableService = mkDisableOption "fwupd service";
+  };
+  config = mkIf cfg.enable {
+    services.fwupd.enable = true;
+    users = setUserGroups admins [ "input" ];
+    systemd.timers.fwupd-refresh.enable = cfg.enableService;
+  };
 })
