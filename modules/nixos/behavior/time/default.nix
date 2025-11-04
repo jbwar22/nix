5f52ccd7005c20abc56869ed9f27bc8b0ab2a925
Jackson Warhover <11669560+jbwar22@users.noreply.github.com>
2025-11-04 18:57:41 +0000

    fix broken timezone

--- modules/nixos/behavior/time/default.nix
+++ modules/nixos/behavior/time/default.nix
@@ -4,5 +4,5 @@ with lib; mkNsEnableModule config ./. {
   services.ntp.enable = true;
   time.timeZone = config.custom.nixos.opts.secrets.timeZone;
 
-  custom.nixos.behavior.impermanence.files = mkIf (config.time.timeZone == null) [ "/etc/localtime" ];
+  # custom.nixos.behavior.impermanence.files = mkIf (config.time.timeZone == null) [ "/etc/localtime" ];
 }
