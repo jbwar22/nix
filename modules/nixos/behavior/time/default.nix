7a968d4fce84041ee4a0724dbe49dd456ba06b6f
Jackson Warhover <11669560+jbwar22@users.noreply.github.com>
2025-11-05 11:26:33 -0500

    timezone bind mount & helper

--- modules/nixos/behavior/time/default.nix
+++ modules/nixos/behavior/time/default.nix
@@ -1,8 +1,22 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
-with lib; mkNsEnableModule config ./. {
+with lib; mkNsEnableModule config ./. (let
+  null-tz = config.time.timeZone == null;
+  imper-e = config.custom.nixos.behavior.impermanence.enable;
+in {
   services.ntp.enable = true;
   time.timeZone = config.custom.nixos.opts.secrets.timeZone;
 
-  # custom.nixos.behavior.impermanence.files = mkIf (config.time.timeZone == null) [ "/etc/localtime" ];
-}
+  custom.nixos.behavior.impermanence.files = mkIf null-tz [ "/etc/localtime" ];
+
+  # timedatectl complains when /etc/localtime is a bind mount or otherwise can't be overwritten
+  # as a fix, we only care about the bind mount at boot. As long as both are updated it's fine
+  environment.systemPackages = mkIf (null-tz && imper-e) (with pkgs; [
+    (writeShellScriptBin "set-timezone" ''
+      sudo umount -q /etc/localtime
+      sudo rm /etc/localtime
+      sudo timedatectl set-timezone $1
+      cat /etc/localtime | sudo tee /persist/etc/localtime > /dev/null
+    '')
+  ]);
+})
