de443e4dc3cc6110d562b3b80a7dc1703153741d
Jackson Warhover <11669560+jbwar22@users.noreply.github.com>
2025-11-18 15:54:11 -0500

    slack womp womp

--- modules/home/programs/slack/default.nix
+++ modules/home/programs/slack/default.nix
@@ -1,12 +1,26 @@
 { config, lib, pkgs, ... }:
 
-with lib; mkNsEnableModule config ./. {
-  home.packages = with pkgs; [
-    (wrapAndAddFlags slack [
-      "--ozone-platform=wayland"
-      "--wayland-text-input-version=3"
-    ])
+with lib; mkNsEnableModule config ./. (let
+  slack = (wrapAndAddFlags pkgs.slack [
+    "--ozone-platform=x11" # wayland often doesn't launch for slack :/
+  ]);
+in {
+  home.packages = [
+    slack
   ];
 
+  xdg.desktopEntries.slack-fix-wayland = {
+    name = "Slack (Wayland Fix)";
+    exec = "${pkgs.writeShellScript "slack-fix-wayland" ''
+      ${slack}/bin/slack --ozone-platform=x11 &
+      ${pkgs.coreutils}/bin/sleep 5
+      ${pkgs.sway}/bin/swaymsg [class=Slack] kill
+      ${pkgs.coreutils}/bin/sleep 1
+      ${pkgs.procps}/bin/pkill -9 slack
+      ${pkgs.coreutils}/bin/sleep 1
+      ${slack}/bin/slack --ozone-platform=wayland $@
+    ''} %u";
+  };
+
   custom.home.behavior.impermanence.dirs = [ ".config/Slack" ];
-}
+})
