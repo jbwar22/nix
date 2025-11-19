9c0f93dce9222016e721c5a83ad2551f9624b09d
Jackson Warhover <11669560+jbwar22@users.noreply.github.com>
2025-11-19 12:27:51 -0500

    remove amdvlk

--- modules/nixos/behavior/graphics/default.nix
+++ modules/nixos/behavior/graphics/default.nix
@@ -6,11 +6,6 @@ with lib; mkNsEnableModule config ./. {
     enable32Bit = true;
   };
 
-  hardware.amdgpu.amdvlk = mkIf (config.custom.common.opts.hardware.gpu == enums.gpu-vendors.amd) {
-      enable = true;
-      support32Bit.enable = true;
-  };
-
   environment.systemPackages = with pkgs; [
     dconf  # gtk
     vulkan-tools
