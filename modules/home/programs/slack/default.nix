bd56dd1c83d815f00fd1d9ca883f393b3e66d3f2
Jackson Warhover <11669560+jbwar22@users.noreply.github.com>
2026-01-19 14:30:55 -0500

    slack back to wayland

--- modules/home/programs/slack/default.nix
+++ modules/home/programs/slack/default.nix
@@ -1,25 +1,18 @@
 { config, lib, pkgs, ... }:
 
 with lib; mkNsEnableModule config ./. (let
-  slack = (wrapAndAddFlags pkgs.slack [
-    "--ozone-platform=x11" # wayland often doesn't launch for slack :/
+  slack = wrapWaylandElectron pkgs.slack;
+  slack-x11 = (wrapAndAddFlags pkgs.slack [
+    "--ozone-platform=x11"
   ]);
 in {
   home.packages = [
     slack
   ];
 
-  xdg.desktopEntries.slack-fix-wayland = {
-    name = "Slack (Wayland Fix)";
-    exec = "${pkgs.writeShellScript "slack-fix-wayland" ''
-      ${slack}/bin/slack --ozone-platform=x11 &
-      ${pkgs.coreutils}/bin/sleep 5
-      ${pkgs.sway}/bin/swaymsg [class=Slack] kill
-      ${pkgs.coreutils}/bin/sleep 1
-      ${pkgs.procps}/bin/pkill -9 slack
-      ${pkgs.coreutils}/bin/sleep 1
-      ${slack}/bin/slack --ozone-platform=wayland $@
-    ''} %u";
+  xdg.desktopEntries.slack-x11 = {
+    name = "Slack (X11 mode)";
+    exec = "${slack-x11}/bin/slack";
   };
 
   custom.home.behavior.impermanence.dirs = [ ".config/Slack" ];
