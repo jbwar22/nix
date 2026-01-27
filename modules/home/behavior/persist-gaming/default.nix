62e5a12b1f7261516e8e096a1491a40cf906ab16
Jackson Warhover <11669560+jbwar22@users.noreply.github.com>
2026-01-27 11:02:41 -0500

    only back up steam game save data

--- modules/home/behavior/persist-gaming/default.nix
+++ modules/home/behavior/persist-gaming/default.nix
@@ -2,8 +2,13 @@
 
 with lib; mkNsEnableModule config ./. {
   custom.home.behavior.impermanence.paths = [
-    ".steam"
+    # very minor stuff, some kind of language setting
+    { path = ".steam"; local = true; }
+
+    # only back up prefixes with game save data
+    { path = ".local/share/Steam"; local = true; }
+    ".local/share/Steam/steamapps/compatdata"
+
     ".config/unity3d"
-    ".local/share/Steam"
   ];
 }
