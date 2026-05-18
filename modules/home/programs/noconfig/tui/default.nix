9c3c0416314d0e13041c4eeec7570f1a7a15ca72
Jackson Warhover <11669560+jbwar22@users.noreply.github.com>
2026-05-18 16:40:05 -0400

    add shorkfetch

--- modules/home/programs/noconfig/tui/default.nix
+++ modules/home/programs/noconfig/tui/default.nix
@@ -7,5 +7,22 @@ ns.enable {
     pulsemixer
     inputs.home-manager.packages.${pkgs.stdenv.hostPlatform.system}.default
     inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default
+
+    (pkgs.stdenv.mkDerivation rec {
+      pname = "shorkfetch";
+      version = "0.1.1";
+      src = pkgs.fetchFromGitHub {
+        owner = "SharktasticA";
+        repo = pname;
+        rev = version;
+        hash = "sha256-yjLeqwEsrBG0S6BjL0QQBVldIG1zm0pcQ4l7IINKK/I=";
+      };
+      nativeBuildInputs = [
+        pkgs.glibc.static
+      ];
+      installPhase = ''
+        make install DESTDIR=$out PREFIX=
+      '';
+    })
   ];
 }
