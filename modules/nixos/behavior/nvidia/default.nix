f1e4bd8b2830bea288926a14ca47b70fcc777b4f
Jackson Warhover <11669560+jbwar22@users.noreply.github.com>
2026-05-27 23:49:37 -0400

    nvidia: rework, create staging specialisation

--- modules/nixos/behavior/nvidia/default.nix
+++ modules/nixos/behavior/nvidia/default.nix
@@ -1,51 +1,56 @@
 { inputs, config, lib, pkgs, ns, ... }:
 
-with lib; ns.enable {
-  specialisation.old.configuration = let
-    kernelPackages = pkgs.linuxPackages_6_18;
-  in {
-    environment.etc.specialisation.text = "6.18, NVIDIA stable";
-    boot.kernelPackages = mkForce kernelPackages;
-    hardware.nvidia.package = mkForce kernelPackages.nvidiaPackages.stable;
+with lib; ns.enable (let
+  mkDriver = config.boot.kernelPackages.nvidiaPackages.mkDriver;
+
+  mkUnstableDriver = branch: pipe inputs.nixpkgs-unstable [
+    (x: x + "/pkgs/os-specific/linux/nvidia-x11/default.nix")
+    readFile
+    (splitString "${branch} = generic {")
+    last
+    (splitString "};")
+    head
+    trim
+    (splitString "\n")
+    (map (x: pipe x [
+      trim
+      (splitString " = ")
+      (x: {
+        name = head x;
+        value = pipe x [
+          last
+          (removePrefix "\"")
+          (removeSuffix "\";")
+        ];
+      })
+    ]))
+    listToAttrs
+    mkDriver
+  ];
+
+  driver-580-142 = mkDriver {
+    version = "580.142";
+    sha256_64bit = "sha256-IJFfzz/+icNVDPk7YKBKKFRTFQ2S4kaOGRGkNiBEdWM=";
+    sha256_aarch64 = "sha256-jntr88SpTYR648P1rizQjB/8KleBoa14Ay12vx8XETM=";
+    openSha256 = "sha256-v968LbRqy8jB9+yHy9ceP2TDdgyqfDQ6P41NsCoM2AY=";
+    settingsSha256 = "sha256-BnrIlj5AvXTfqg/qcBt2OS9bTDDZd3uhf5jqOtTMTQM=";
+    persistencedSha256 = "sha256-il403KPFAnDbB+dITnBGljhpsUPjZwmLjGt8iPKuBqw=";
+  };
+
+  # define drivers
+  stagingDriver = mkUnstableDriver "production";
+  stableDriver = driver-580-142;
+in {
+
+  specialisation.nvidia-staging.configuration = {
+    environment.etc.specialisation.text = "NVIDIA staging";
+    hardware.nvidia.package = mkForce stagingDriver;
   };
 
   services.xserver.videoDrivers = ["nvidia"];
 
-  hardware.nvidia = let
-    driverAttrsParsed = pipe inputs.nixpkgs-unstable [
-      (x: x + "/pkgs/os-specific/linux/nvidia-x11/default.nix")
-      readFile
-      (splitString "production = generic {")
-      last
-      (splitString "};")
-      head
-      trim
-      (splitString "\n")
-      (map (x: pipe x [
-        trim
-        (splitString " = ")
-        (x: {
-          name = head x;
-          value = pipe x [
-            last
-            (removePrefix "\"")
-            (removeSuffix "\";")
-          ];
-        })
-      ]))
-      listToAttrs
-    ];
-    driverAttrsDefined = {
-      version = "580.142";
-      sha256_64bit = "sha256-IJFfzz/+icNVDPk7YKBKKFRTFQ2S4kaOGRGkNiBEdWM=";
-      sha256_aarch64 = "sha256-jntr88SpTYR648P1rizQjB/8KleBoa14Ay12vx8XETM=";
-      openSha256 = "sha256-v968LbRqy8jB9+yHy9ceP2TDdgyqfDQ6P41NsCoM2AY=";
-      settingsSha256 = "sha256-BnrIlj5AvXTfqg/qcBt2OS9bTDDZd3uhf5jqOtTMTQM=";
-      persistencedSha256 = "sha256-il403KPFAnDbB+dITnBGljhpsUPjZwmLjGt8iPKuBqw=";
-    };
-    driverAttrs = driverAttrsDefined;
-  in {
-    package = config.boot.kernelPackages.nvidiaPackages.mkDriver driverAttrs;
+  hardware.nvidia = {
+    package = stableDriver;
     open = true;
 
     modesetting.enable = true;
@@ -63,4 +68,4 @@ with lib; ns.enable {
     extraModprobeConfig = "options nvidia-drm";
     blacklistedKernelModules = [ "nouveau" ];
   };
-}
+})
