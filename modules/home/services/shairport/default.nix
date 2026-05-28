3d72de9a0f222d862049151b8fd373622d71fafb
Jackson Warhover <11669560+jbwar22@users.noreply.github.com>
2026-05-28 01:36:48 -0400

    shairport: password optimization

--- modules/home/services/shairport/default.nix
+++ modules/home/services/shairport/default.nix
@@ -19,13 +19,22 @@ with lib; with clib; with ns; {
         After = "pipewire.target";
       };
       Service = {
+        PrivateTmp = true;
         ExecStart = let
           password-file = ageOrNull config "shairport-password";
-          password-arg = if password-file == null then "" else "--password=\"$(${pkgs.coreutils}/bin/cat ${password-file})\"";
-        in pkgs.writeShellScript "shairport-sync" ''
-          ${pkgs.shairport-sync}/bin/shairport-sync -o pulseaudio -p ${toString cfg.port} \
-          -a "${capitalizeDashedString config.custom.common.opts.host.hostname}" ${password-arg}
-        '';
+        in pkgs.writeShellScript "shairport-sync" (if password-file == null then ''
+          ${pkgs.shairport-sync}/bin/shairport-sync \
+          -o pulseaudio \
+          -p ${toString cfg.port} \
+          -a "${capitalizeDashedString config.custom.common.opts.host.hostname}"
+        '' else ''
+          ${pkgs.gnused}/bin/sed 's/.*/general = { password = "&"; }/' ${password-file} > /tmp/sp.conf
+          ${pkgs.shairport-sync}/bin/shairport-sync \
+          -o pulseaudio \
+          -p ${toString cfg.port} \
+          -a "${capitalizeDashedString config.custom.common.opts.host.hostname}" \
+          -c /tmp/sp.conf
+        '');
       };
       Install = {
         WantedBy = [ "default.target" ];
