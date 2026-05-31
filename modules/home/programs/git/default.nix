7832de314e46566f7629453b7a3744641691a335
Jackson Warhover <11669560+jbwar22@users.noreply.github.com>
2026-05-31 19:27:04 -0400

    git: use home-manager module

--- modules/home/programs/git/default.nix
+++ modules/home/programs/git/default.nix
@@ -2,11 +2,25 @@
 
 ns.enable {
   home.packages = with pkgs; [
-    git
     git-crypt
   ];
 
-  custom.home.behavior.impermanence.paths = [
-    { path = ".gitconfig"; file = true; }
-  ];
+  programs.git = {
+    enable = true;
+    signing = {
+      format = "ssh";
+      signByDefault = true;
+    };
+    settings = {
+      init.defaultBranch = "main";
+      alias = let
+        loggFlags = "--graph --pretty=tformat:'%Cred%h %Cgreen%cd %C(bold blue)%an%Creset%C(yellow)%d%Creset %s' --date=short";
+      in {
+        logg = "log ${loggFlags}";
+        flogg = "log ${loggFlags} --all";
+        reflogg = "log --reflog ${loggFlags} --all";
+        bigblame = "! git ls-files | xargs -n1 git blame --line-porcelain 2>/dev/null | sed -n 's/^author //p' | sort -f | uniq -ic | sort -nr";
+      };
+    };
+  };
 }
